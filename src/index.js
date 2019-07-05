c48761e193c7e0010d9916f8e4d9d707450ea3db
mabry1985 <mabry1985@gmail.com>
2019-07-05 11:01:19 -0700

    add logic to add name, address, etc to DOM from api request

--- src/index.js
+++ src/index.js
@@ -15,8 +15,16 @@ $(document).ready(function () {
     promise.then(function (response) {
       let body = JSON.parse(response);
 
+      if (body.data.length === 0) {
+        $('.doctor-name').append('There are no doctors matching this search, please try again');
+      }
+
       for (let i = 0; i < body.data.length; i++) {
-        $('.doctor-name').append(`${body.data[i].profile.first_name} ${body.data[i].profile.last_name}`);
+        $('.doctor-name').append(`<strong>Name:</strong> ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}<br>
+        <strong>Address:</strong> ${body.data[i].practices[0].visit_address.street}<br>
+        <strong>Number:</strong> ${body.data[i].practices[0].phones[0].number}<br>
+        <strong>Website: </strong> ${body.data[i].practices[0].website} <br>
+        <strong>Accepting New Patients:</strong> ${body.data[i].practices[0].accepts_new_patients}<br> <br>`);
       }
     }, function (error) {
 
