60cc9a2c4418fbf4eb905d5fef167e5c2b563d12
mabry1985 <mabry1985@gmail.com>
2019-07-05 15:13:27 -0700

    add border to html and adjust api to only allow 10max search results at a time

--- src/doctor.js
+++ src/doctor.js
@@ -2,7 +2,7 @@ export class BetterDoctor {
   doctorSearch(docName, condition) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${docName}&query=${condition}&location=or-portland&skip=0&limit=25&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${docName}&query=${condition}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
