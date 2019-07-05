8d3fd71707218ee2cae93d57a4b50de98a363374
mabry1985 <mabry1985@gmail.com>
2019-07-05 09:48:56 -0700

    try to add api url

--- src/weather-service.js
+++ src/weather-service.js
@@ -1,9 +1,8 @@
 export class BetterDoctor {
   getWeatherByCity(city) {
-    const key = process.env.exports.apiKey;
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=688ccf81582f9e912922ff138e75a0d3`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${drName}&query=${condition}&location=or-portland&user_location=45.5155%2C%20-122.6793&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
