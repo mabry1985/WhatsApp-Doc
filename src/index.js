d836346f5967703bcd9a6a86b66d4a6cc51a7f4f
mabry1985 <mabry1985@gmail.com>
2019-07-05 11:09:41 -0700

    add logic to erase DOM of previous results on submit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 $(document).ready(function () {
   $('.doc-form').submit(function (event) {
     event.preventDefault();
+    $('.doctor-info').empty();
     let nameInput = $('#name').val();
     let conditionInput = $('#condition').val();
     $('#name').val('');
@@ -16,11 +17,11 @@ $(document).ready(function () {
       let body = JSON.parse(response);
 
       if (body.data.length === 0) {
-        $('.doctor-name').append('There are no doctors matching this search, please try again');
+        $('.doctor-info').append('There are no doctors matching this search, please try again');
       }
 
       for (let i = 0; i < body.data.length; i++) {
-        $('.doctor-name').append(`<strong>Name:</strong> ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}<br>
+        $('.doctor-info').append(`<strong>Name:</strong> ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}<br>
         <strong>Address:</strong> ${body.data[i].practices[0].visit_address.street}<br>
         <strong>Number:</strong> ${body.data[i].practices[0].phones[0].number}<br>
         <strong>Website: </strong> ${body.data[i].practices[0].website} <br>
@@ -28,7 +29,7 @@ $(document).ready(function () {
       }
     }, function (error) {
 
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      $('.show-errors').text(`There was an error processing your request: ${error.message}`);
     });
   });
 
